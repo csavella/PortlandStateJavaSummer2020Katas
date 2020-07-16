8029d113f65272cb5adddd3bf9af7fd3372be04f
京子 <ajw29@pdx.edu>
2020-07-15 21:05:06 -0700

    Update Diamond.java
    added some stuff

--- Diamond/src/main/java/edu/pdx/cs410J/csavella/Diamond.java
+++ Diamond/src/main/java/edu/pdx/cs410J/csavella/Diamond.java
@@ -23,9 +23,27 @@ public class Diamond {
     int asciiValueA = 65;
     int diff = asciiValue - asciiValueA;
 
-    String inner, outer;
-    for(int i = 0; i <= diff; ++i) {
+    String inner = "", outer = "";
+    for(int i = 0; i < diff; i++){
+      outer += " ";
+    }
 
+    int innerSpace = diff;
+    for(int i = 0; i <= diff; ++i) {
+      if(i == 0){
+        diamondList.add(outer + "A" + outer);
+        inner = " ";
+        outer = outer.substring(0, outer.length() - 1);
+
+      }else{
+        diamondList.add(outer + ((char)(asciiValueA + i)) + inner + ((char)(asciiValueA + i)) + outer );
+        inner += "  ";
+        outer = outer.substring(0, outer.length() - 2);
+      }
+
+      for (String s: diamondList){
+        System.out.println(s);
+      }
     }
 
     return null;
