be6fe7ded56d888ec3431e2dfd57994bee104715
Chasity Savella <csavella@pdx.edu>
2020-07-15 19:37:49 -0700

    Create Diamond Katas

---
+++ Diamond/src/main/java/edu/pdx/cs410J/csavella/Kata.java
@@ -0,0 +1,15 @@
+package edu.pdx.cs410J.csavella;
+
+/**
+ * A class for getting started with a code kata
+ *
+ * Use IntelliJ's "Refactor | Rename..." command to change the name of this
+ * class (and its tests).
+ */
+public class Kata {
+                                                                                    
+
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+  }
+}
---
+++ Diamond/src/test/java/edu/pdx/cs410J/csavella/KataTest.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs410J.csavella;
+
+import org.junit.Test;
+
+public class KataTest
+{
+
+  @Test
+  public void canInstantiateKataClass() {
+    new Kata();
+  }
+
+}
