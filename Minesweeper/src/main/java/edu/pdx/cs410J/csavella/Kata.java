5110d21421e3a38852aedccc16349d0e633aef93
Theo Lee <theo.e.lee@gmail.com>
2020-07-29 20:06:17 -0700

    Populated board with mines. Started implementing mine count

--- Minesweeper/src/main/java/edu/pdx/cs410J/csavella/Kata.java
+++ Minesweeper/src/main/java/edu/pdx/cs410J/csavella/Kata.java
@@ -1,5 +1,7 @@
 package edu.pdx.cs410J.csavella;
 
+import java.util.Scanner;
+
 /**
  * A class for getting started with a code kata
  *
@@ -7,19 +9,23 @@ package edu.pdx.cs410J.csavella;
  * class (and its tests).
  */
 public class Kata {
-  public char[][] getMinesweeper(int length, int width) {
+
+  private static char[][] minesweeper;
+  static int length;
+  static int width;
+
+  public static void getMinesweeper() {
     char[][] minesweeper = new char[length][width];
     for (int i = 0; i < width; i++) {
       for (int j = 0; j < length; j++) {
         minesweeper[i][j] = '.';
       }
     }
-    return minesweeper;
+    //return minesweeper;
   }
                                                                                     
 
   public static void main(String[] args) {
-    int length, width;
 
     if (args.length != 2) {
       System.err.println("Missing command line arguments.");
@@ -27,5 +33,67 @@ public class Kata {
 
     length = Integer.parseInt( args[0] );
     width = Integer.parseInt( args[1] );
+
+    getMinesweeper();
+    populateMines();
+
+  }
+
+  public static void populateMines(){
+    String answer;
+    int row, col;
+    Scanner read = new Scanner(System.in);
+
+    do
+    {
+      System.out.print("Enter in two numbers to populate a mine in that index: ");
+      row = read.nextInt();
+      col = read.nextInt();
+
+      minesweeper[row][col] = '*';
+
+      System.out.println("Would you like to enter another mine? (y/n): ");
+      answer = read.next();
+
+    } while (answer =="y");
+  }
+
+  public static void calculateOutput()
+  {
+    for (int i = 0; i < length; ++i)
+    {
+      for (int j = 0; j < width; ++j)
+      {
+
+        checkForMines(length, width);
+
+
+
+
+
+
+
+
+      }
+    }
+  }
+
+  private static void checkForMines(int row, int col)
+  {
+    int count = 0;
+
+    // [0, 0]
+    if (row - 1 < 0 && col - 1 < 0)
+    {
+      if (minesweeper[row + 1][col] == '*')
+        ++count;
+
+      if (minesweeper[])
+
+    }
+
+
+
+
   }
 }
